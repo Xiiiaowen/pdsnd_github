c69be103b1824e2ebbf19c5921c11ee9161650d0
xiaowen <sxwzdzq@gmail.com>
2025-10-16 23:00:29 +0200

    Update bikeshare.py Remove 5 question details

--- bikeshare.py
+++ bikeshare.py
@@ -1,30 +1,7 @@
 """
 Exploring US Bike Share Data
-Project Overview This project analyzes bike share system data from three major US cities: Chicago, New York City, and Washington, DC. Using data provided by Motivate, we will uncover usage patterns and compare system behavior across these cities.
-The original, messier datasets have been wrangled into a consistent format for this analysis by Udacity: chicago.csv, new_york_city.csv, washington.csv
-
-Key Questions
-
-1. Popular Times of Travel:
-Most common month
-Most common day of the week
-Most common hour of the day
-2. Popular Stations and Trip:
-Most common start station
-Most common end station
-Most common trip (start to end station combination)
-3. Trip Duration:
-Total travel time
-Average travel time
-4. User Information:
-Counts of each user type (Subscriber/Customer)
-Counts of each gender (only for NYC and Chicago)
-Earliest, most recent, and most common year of birth (only for NYC and Chicago)
-
-5. Subscriber Information:
-Count of each gender
-Count of Birth Year Groups (<1980, 1980-1989, 1990-1999, 2000-2009, >2010)
-Statistics about Trip Duration (average, max, min)
+Project Overview 
+This project analyzes bike share system data from three major US cities: Chicago, New York City, and Washington, DC. Using data provided by Motivate, we will uncover usage patterns and compare system behavior across these cities.
 """
 
 # 0. Import library and define some functions that could be reusable for later analysis
